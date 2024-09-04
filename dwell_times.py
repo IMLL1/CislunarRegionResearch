7c12af14e6d17471ba75b65e5c19e02d69affd8b
Polaris <imll1@icloud.com>
2024-09-04 16:53:22 -0400

    three methods implemented so far

--- dwell_times.py
+++ dwell_times.py
@@ -1,5 +1,8 @@
 import numpy as np
 from colorutils import hsv_to_hex
+from scipy.stats import gaussian_kde
+import matplotlib.pyplot as plt
+
 
 # NOTE: states is N x 6
 
@@ -20,14 +23,12 @@ def r_rate(states_rotating: list, periods: list, earth_x: float, ids: list):
         vel_run = vel[n]
         radius_run = np.linalg.vector_norm(pos_run, axis=1)
         radial_vel = pos_run * np.sum(pos_run * vel_run) / radius_run[:, None] ** 2
-        r_rate.append(np.abs(radial_vel/periods[n]))
+        r_rate.append(np.abs(radial_vel / periods[n]))
         radius.append(radius_run)
     # return radius, dwell_time
-    import matplotlib.pyplot as plt
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    lines = []
     for idx in range(n):
         ax.plot(
             radius[idx],
@@ -38,6 +39,91 @@ def r_rate(states_rotating: list, periods: list, earth_x: float, ids: list):
     ax.set_ylabel("Dwell Time [km/period]")
     ax.set_xlabel("Radius [km]")
     plt.title("Dwell Time vs Radius")
+    plt.grid(linestyle="dashed", lw=0.5, c="gray")
+    fig.legend()
+    plt.show()
+
+
+def hist_linear_density(
+    states_rotating: list,
+    times: list,
+    periods: list,
+    earth_x: float,
+    ids: list,
+    num_points: int = 1e7,
+):
+    num_points = int(num_points)
+    pos = [
+        states_run[:, :3] - np.array([earth_x, 0, 0]) for states_run in states_rotating
+    ]
+    radius = []
+    pdfs = []
+    for n in range(len(pos)):
+        pos_run = pos[n]
+        t = np.linspace(0, periods[n], num_points)
+        pos_interp = np.array(
+            [np.interp(t, times[n], pos_run[:, dim]) for dim in range(3)]
+        ).T
+        radius_run = np.linalg.vector_norm(pos_interp, axis=1)
+        radius.append(radius_run)
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    for idx in range(n):
+        y, x = np.histogram(
+            radius_run, bins=max([num_points // 5000, 100]), density=True
+        )
+        x = np.mean([x[1:], x[:-1]], axis=0)
+        ax.plot(
+            x,
+            y,
+            color=hsv_to_hex((360 * idx / len(pos), 0.75, 1)),
+            label="id: " + str(ids[idx]),
+        )
+    ax.set_ylabel("Probability Density")
+    ax.set_xlabel("Radius [km]")
+    plt.title("Probability vs Radius")
+    plt.grid(linestyle="dashed", lw=0.5, c="gray")
+    fig.legend()
+    plt.show()
+
+
+def kern_linear_density(
+    states_rotating: list,
+    times: list,
+    periods: list,
+    earth_x: float,
+    ids: list,
+    num_points: int = 1e6,
+):
+    num_points = int(num_points)
+    pos = [
+        states_run[:, :3] - np.array([earth_x, 0, 0]) for states_run in states_rotating
+    ]
+    radius = []
+    pdfs = []
+    for n in range(len(pos)):
+        pos_run = pos[n]
+        t = np.linspace(0, periods[n], num_points)
+        pos_interp = np.array(
+            [np.interp(t, times[n], pos_run[:, dim]) for dim in range(3)]
+        ).T
+        radius_run = np.linalg.vector_norm(pos_interp, axis=1)
+        radius.append(radius_run)
+        pdfs.append(gaussian_kde(radius_run))
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    for idx in range(n):
+        radii = np.linspace(min(radius[idx]), max(radius[idx]), int(5e2))
+        pdf_val = pdfs[idx].pdf(radii)
+        ax.plot(
+            radii,
+            pdf_val,
+            color=hsv_to_hex((360 * idx / len(pos), 0.75, 1)),
+            label="id: " + str(ids[idx]),
+        )
+    ax.set_ylabel("Probability Density")
+    ax.set_xlabel("Radius [km]")
+    plt.title("Probability vs Radius")
     plt.grid(linestyle="dashed", lw=0.5, c="gray")
     fig.legend()
     plt.show()
--- main.py
+++ main.py
@@ -2,28 +2,40 @@ from CR3BP_propagator import CR3BP
 from read_ICs import read_ICs
 from plot_trajectories import plot_trajectories
 from numpy.random import seed
-from dwell_times import r_rate
+from dwell_times import r_rate, hist_linear_density, kern_linear_density
+
+# Python docs used extensively
 
 seed(0)
 
 # ids = [25, 50, 75, 100, 150, 200, 250, 300, 400, 500]
-ids = [10, 25, 100, 500, 1000]
+ids = [500, 500]  # [10, 25, 100, 500, 1000]
 tf, DRO_ICs = read_ICs(ids)
 base_propagator = CR3BP(LU=389703, TU=382981)
 
 inertial_states = []
 CR3BP_states = []
+times = []
 for idx in range(len(ids)):
     base_propagator.propagate(DRO_ICs[idx, :], tf[idx])
     base_propagator.get_inertial_states()
-    inertial = base_propagator.states
-    cr3bp = base_propagator.states_cr3bp
-    inertial_states.append(inertial)
-    CR3BP_states.append(cr3bp)
+    inertial_states.append(base_propagator.states)
+    CR3BP_states.append(base_propagator.states_cr3bp)
+    times.append(base_propagator.ts)
 
-r_rate(
+# Dwell Time functions
+
+# r_rate(
+#     CR3BP_states,
+#     tf,
+#     (-base_propagator.mu) * base_propagator.LU,
+#     ids,
+# )
+
+hist_linear_density(
     CR3BP_states,
-    tf,
+    times,
+    tf * base_propagator.TU,
     (-base_propagator.mu) * base_propagator.LU,
     ids,
 )
